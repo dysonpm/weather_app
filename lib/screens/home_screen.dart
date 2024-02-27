6a1c2846da670432dacef16256f3e25163a72ff2
Dieson <97970907+dysonpm@users.noreply.github.com>
2024-02-27 19:27:59 +0100

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,11 +8,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         systemOverlayStyle:
             const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 1 * kToolbarHeight, 40, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: const Color.fromARGB(255, 15, 85, 143)),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
