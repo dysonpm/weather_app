b57763adcce0997c05b824301eee89a063c8ab12
Dieson <97970907+dysonpm@users.noreply.github.com>
2024-02-29 20:47:40 +0100

    bloc update

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,12 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather/weather.dart';
 
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
 
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) {
+    on<FetchWeather>((event, emit) {
       // TODO: implement event handler
     });
   }
--- lib/bloc/weather_bloc_state.dart
+++ lib/bloc/weather_bloc_state.dart
@@ -2,9 +2,26 @@ part of 'weather_bloc_bloc.dart';
 
 sealed class WeatherBlocState extends Equatable {
   const WeatherBlocState();
-  
+
   @override
   List<Object> get props => [];
 }
 
+// Definiert einen initialen Zustand für den WeatherBloc.
 final class WeatherBlocInitial extends WeatherBlocState {}
+
+//Definiert einen Ladezustand für den WeatherBloc.
+final class WeatherBlocLoading extends WeatherBlocState {}
+
+// Definiert einen Fehlerzustand für den WeatherBloc
+final class WeatherBlocFailure extends WeatherBlocState {}
+
+// Definiert einen Erfolgszustand für den WeatherBloc
+final class WeatherBlocSuccess extends WeatherBlocState {
+  final Weather weather;
+
+  const WeatherBlocSuccess(this.weather);
+
+  @override
+  List<Object> get props => [weather];
+}
